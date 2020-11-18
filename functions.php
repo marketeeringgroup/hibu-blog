d07bba68d8c4eb5007c73d74063324f2ad7cfee2
Melissa Wong <mellymai@gmail.com>
2020-11-18 10:55:13 -0800

    adds jquery dependency to custom.js

--- functions.php
+++ functions.php
@@ -6,3 +6,8 @@ function hibu_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'hibu_enqueue_styles' );
 
+// Enqueue custom js
+function hibu_enqueue_scripts() { 
+    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js', array('jquery') );
+}
+add_action( 'wp_enqueue_scripts', 'hibu_enqueue_scripts' );
